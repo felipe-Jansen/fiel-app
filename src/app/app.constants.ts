a005c982ea0727b2feaec02c71e4275870d95476
Felipe Jansen <felipe.jansen@outlook.com>
2020-03-25 22:08:02 -0300

    v0.0.4

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:8080';
-export const API_URL_V1 = 'http://localhost:8080/api/v1';
+// export const API_URL = 'http://localhost:8080';
+// export const API_URL_V1 = 'http://localhost:8080/api/v1';
 //
-// export const API_URL = 'http://3.88.91.107';
-// export const API_URL_V1 = 'http://3.88.91.107/api/v1';
+export const API_URL = 'http://54.91.216.44';
+export const API_URL_V1 = 'http://54.91.216.44/api/v1';
